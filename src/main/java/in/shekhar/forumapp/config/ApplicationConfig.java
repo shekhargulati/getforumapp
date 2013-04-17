0771fd2445b06103895d1b10dfce5f35eb7f58ca
Shekhar Gulati <shekhargulati84@gmail.com>
2013-04-17 14:40:42 +0530

    Step 2 : Application configuration bootstraping

---
+++ src/main/java/in/shekhar/forumapp/config/ApplicationConfig.java
@@ -0,0 +1,45 @@
+package in.shekhar.forumapp.config;
+
+import in.shekhar.forumapp.domain.Thread;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@ComponentScan(basePackages = "in.shekhar.forumapp")
+@Configuration
+public class ApplicationConfig {
+
+	@Autowired
+	private DataSourceConfig dataSourceConfig;
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		vendorAdapter.setDatabase(dataSourceConfig.database());
+		vendorAdapter.setGenerateDdl(true);
+		vendorAdapter.setShowSql(true);
+
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		factory.setJpaVendorAdapter(vendorAdapter);
+		factory.setPackagesToScan(Thread.class.getPackage().getName());
+		factory.setDataSource(dataSourceConfig.dataSource());
+
+		return factory;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+
+		JpaTransactionManager txManager = new JpaTransactionManager();
+		txManager.setEntityManagerFactory(entityManagerFactory().getObject());
+		return txManager;
+	}
+	
+}
